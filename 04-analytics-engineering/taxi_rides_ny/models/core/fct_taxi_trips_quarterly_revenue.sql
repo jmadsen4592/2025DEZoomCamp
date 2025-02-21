4e327c771a1d5c231c96eb3bf271629d0005efb2
John Madsen <jmadsen45@gmail.com>
2025-02-21 21:39:29 +0000

    question5test

--- 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
@@ -2,12 +2,29 @@
 
 with trips_data as (
     select * from {{ ref('fact_trips') }}
+),
+    quart_rev as (
+    select
+    service_type, 
+    sum(total_amount) as quarterly_revenue,
+    year_quarter,
+    year,
+    quarter
+
+    from trips_data
+
+    WHERE year in (2019, 2020)
+    
+    group by service_type, year_quarter, year, quarter
 )
 
     select 
-    sum(total_amount) as quarterly_revenue,
-    year_quarter 
+    y2019.service_type,
+    y2019.quarterly_revenue as rev2019,
+    y2020.quarterly_revenue as rev2020,
+    y2019.quarter,
+    (y2020.quarterly_revenue-y2019.quarterly_revenue)/y2019.quarterly_revenue*100 as YoY_rev_growth
+    from quart_rev y2019
+    join quart_rev y2020 on y2020.quarter = y2019.quarter
+    order by y2019.quarter
 
-    from trips_data
-    
-    group by year_quarter
