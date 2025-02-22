90ecf42f550093ece5f053029a4963d901258319
John Madsen <jmadsen45@gmail.com>
2025-02-22 00:01:24 +0000

    hw4 question 6 try 1

---
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
@@ -0,0 +1,20 @@
+{{ config(materialized='table') }}
+
+with trips_data as (
+    select * from {{ ref('fact_trips') }}
+)
+select
+    service_type,
+    year,
+    month,
+    PERCENTILE_CONT(fare_amount, .97) OVER(PARTITION BY service_type, year, month) as p97,
+    PERCENTILE_CONT(fare_amount, .95) OVER(PARTITION BY service_type, year, month) as p95,
+    PERCENTILE_CONT(fare_amount, .90) OVER(PARTITION BY service_type, year, month) as p90
+FROM 
+    trips_data
+WHERE 
+    fare_amount > 0
+    and trip_distance > 0
+    and payment_type_description in ('Cash', 'Credit Card')
+ORDER BY
+    1,2,3
